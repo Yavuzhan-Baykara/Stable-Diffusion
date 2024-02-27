6efc5957d308d04008004d74486d1f5c9d16a59d
YavuzhanBaykara <yavuzhan96@gmail.com>
2024-02-27 09:37:13 +0300

    unetinit

--- models.py
+++ models.py
@@ -5,16 +5,64 @@ import json
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import functools
 
 
 class GaussianFourierProjection(nn.Module):
     def __init__(self, embed_dim, scale=30.):
         super().__init__()
-
         self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
     
     def forward(self, x):    
         x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
-
         return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
+class Dense(nn.Module):
+    def __init__(self, input_dim, output_dim) -> None:
+        super().__init__()
+        self.dense = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.dense(x)[..., None, None]
+    
+class Unet(nn.Module):
+    def __init__(self, marginal_prob_std, embed_dim=256, channels=[32, 64, 128, 256]):
+        super().__init__()
+        self.time_embed = nn.Sequential(
+            GaussianFourierProjection(embed_dim=embed_dim),
+            nn.Linear(embed_dim, embed_dim)
+        )
+
+        self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
+        self.dense1 = Dense(embed_dim, channels[0])
+        self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+
+        self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
+        self.dense2 = Dense(embed_dim, channels[1])
+        self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+
+        self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+        self.dense3 = Dense(embed_dim, channels[2])
+        self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+        
+        self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+        self.dense4 = Dense(embed_dim, channels[3])
+        self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])
+
+        self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False)
+        self.dense5 = Dense(embed_dim, channels[2])
+        self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+
+        self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False, output_padding=1)
+        self.dense6 = Dense(embed_dim, channels[1])
+        self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+
+        self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False, output_padding=1)
+        self.dense7 = Dense(embed_dim, channels[0])
+        self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+
+        self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
+
+        self.act = lambda x: x * torch.sigmoid(x)
+        self.marginall_prob_std = marginal_prob_std
     
